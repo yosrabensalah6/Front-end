d5bb18b6c622dd617ce9b2f1ec62388e60f8ee6d
yosrabensalah6 <yosrabensalah@hotmail.fr>
2020-05-14 22:14:04 +0100

    add patient  service

--- src/app/modules/services/patient.service.ts
+++ src/app/modules/services/patient.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Observable} from "rxjs";
-import {AppointmentModel} from "../modeles/Appointment.model";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 import {Patient} from "../modeles/Patient.model";
 
 @Injectable({
